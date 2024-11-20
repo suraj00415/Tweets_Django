ae52bdb087326e177150b8d204fb3c235b1f5b43
Suraj <suraj00415@gmail.com>
2024-11-20 11:30:02 +0530

    django project

---
+++ tweetsHQ/tweet/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.tweet_list,name='tweet_list'),
+    path('tweet_create/',views.tweet_create,name='tweet_create'),
+    path('tweet_delete/<int:tweet_id>/',views.tweet_delete,name='tweet_delete'),
+    path('tweet_edit/<int:tweet_id>/',views.tweet_edit,name='tweet_edit'),
+    path('register/',views.register,name='register'),
+]
